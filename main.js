4eb0361b4794109a33b7fc3c1ae89ab7ded261d6
linlinzzo(林林) <79007796+linlinzzo@users.noreply.github.com>
2022-02-26 16:39:45 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  // 声明变量
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+ 
+  // 循环遍历所有列表项，并隐藏那些与搜索查询不匹配的项
+  for (i=0; i<tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}

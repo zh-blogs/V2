f5a28242b89b963210e3ae0e8af962eafb193275
linlinzzo(林林) <79007796+linlinzzo@users.noreply.github.com>
2022-03-14 10:13:38 +0800

    Delete shuiji.js

--- shuiji.js
+++
@@ -1,8 +0,0 @@
-           "https://coolshell.cn/",
-            "https://alili.tech/",
-            "https://yanhaijing.com/",
-            "https://justjavac.com/",
-            "https://xuexb.com/",
-            "https://imququ.com/",
-            "https://www.xiabingbao.com/",
-              "http://www.weibalu.com/"

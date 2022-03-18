da8a49d587fb12b766364ec84175b3bcdfa34de4
linlinzzo(林林) <79007796+linlinzzo@users.noreply.github.com>
2022-03-18 17:02:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -63,17 +63,13 @@ function BlogList(app, data) {
     const content = pageList
       .map((item) =>
         item === -3
-          ? `<button type='button' class='btn btn-outline-dark prePage' ${
+          ? `<ul class="pagination"><button type='button' class='btn btn-outline-dark prePage' ${
               curPage === 0 ? 'disabled' : ''
-            }><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-left" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
-</svg></button>`
+            }>&laquo;</button>`
           : item === -2
           ? `<button type='button' class='btn btn-outline-dark nextPage' ${
               curPage === totalPage - 1 ? 'disabled' : ''
-            }><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
-</svg></button>`
+            }>&raquo;</button></ul>`
           : item === -1
           ? `<div>...</div>`
           : `<button type='button' class='btn btn-outline-dark jump${

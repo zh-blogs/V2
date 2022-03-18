4b57718568239dcbe3dac51b4870301a30f898c0
Mayx <i@mayx.eu.org>
2022-03-18 21:50:01 +0800

    网站测试脚本

---
+++ sitetest.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+with open('data.json', 'r', encoding='utf8') as json_file:
+    json_data = json.load(json_file)
+    
+for index in range(len(json_data)):
+    print("Checking:", json_data[index]["url"], end = " ")
+    try:
+        res = requests.get(json_data[index]["url"])
+        if res.status_code == 200:
+            json_data[index]["status"] = "OK"
+            print("[ OK ]")
+        else:
+            json_data[index]["status"] = res.status_code
+            print("[", res.status_code,"]")
+    except Exception as e:
+        json_data[index]["status"] = str(type(e))
+        print("[", str(type(e)),"]")
+
+with open('data.json', 'w', encoding='utf8') as json_file:
+    json_file.write(json.dumps(json_data, ensure_ascii=False))

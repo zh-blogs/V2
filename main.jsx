faccb602ef4e77ee6675b09dced73a1a67471f80
linlinzzo(林林) <linlinabcb.icoc@foxmail.com>
2022-03-27 10:37:26 +0800

    fix: github repo url

--- main.jsx
+++ main.jsx
@@ -673,7 +673,7 @@
         ) : (
           <div className='alert alert-info alert-dismissible fade show'>
             <strong>抱歉</strong>我们没能搜索到符合条件的博客。你可以尝试
-            <a href='https://github.com/linlinzzo/blog-daohang/issues/new?assignees=linlinzzo&labels=&template=add.md&title=%E7%94%B3%E8%AF%B7%E6%B7%BB%E5%8A%A0%E7%BD%91%E7%AB%99'>
+            <a href='https://github.com/zh-blogs/blog-daohang/issues/new?assignees=linlinzzo&labels=&template=add.md&title=%E7%94%B3%E8%AF%B7%E6%B7%BB%E5%8A%A0%E7%BD%91%E7%AB%99'>
               提交它
             </a>
             。

a705a2e23ccb6925c46ed06c2fc19ef307dd8b9a
OhYee <oyohyee@oyohyee.com>
2022-06-13 01:21:50 +0800

    fix: join_time and update_time error

--- utils/backend/db.ts
+++ utils/backend/db.ts
@@ -211,7 +211,7 @@ async function updateBlog(params: { id: string, blog: Blog }): Promise<Result> {
         update_time: now,
       };
       for (const key of Object.keys(obj) as (keyof Blog)[]) {
-        oldBlog[key] = params.blog[key] as never;
+        oldBlog[key] = obj[key] as never;
       }
     }
   );

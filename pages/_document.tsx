6979e7a7b98a7165d1fd14a46813e31a8cf208ef
Redish101 <jiayunluo@outlook.com>
2022-06-24 05:58:06 +0800

    feat: 尝试支持Service Worker

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
           <meta httpEquiv="Cache-Control" content="no-transform" />
 
           <link rel="canonical" href="https://zhblogs.ohyee.cc/" />
-
+          
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>

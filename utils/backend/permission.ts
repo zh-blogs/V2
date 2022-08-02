dec237c8bf8536dba328b11482bfc3943b754f12
Justin Sun <i@jsun969.cn>
2022-08-02 23:12:29 +0800

    feat: remove some old files

--- utils/backend/permission.ts
+++
@@ -1,7 +0,0 @@
-import DB from "@/utils/backend/db";
-
-export async function checkPermission(token: string) {
-  const permission = await DB.getUser({ token });
-
-  return !!permission.success && !!permission.data && !!permission.data.admin;
-}
--- utils/moment.ts
+++
@@ -1,5 +0,0 @@
-import moment from "moment";
-
-moment.locale("zh-cn");
-
-export default moment;

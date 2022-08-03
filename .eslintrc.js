d455ebd2f9f22a6795ebfc908313eb0944225846
Justin Sun <i@jsun969.cn>
2022-08-03 16:37:59 +0800

    chore: 添加prettier和eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ['eslintrc.js', 'dist/*'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'no-console': 'error',
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+};

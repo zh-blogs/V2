887e94b651600db24867b119ae679006bbfa5cfd
Justin Sun <i@jsun969.cn>
2022-08-04 23:21:51 +0800

    feat: 添加按钮悬停与按下样式

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 
 const Button = ({ icon, children }: ButtonProps) => {
   return (
-    <button className="w-[90px] h-[25px] bg-[#40A9FF] text-white text-xs leading-6 shadow-md">
+    <button className="w-[90px] h-[25px] transition-colors bg-[#40A9FF] hover:bg-[#2B699C] active:bg-[#8CCBFF] text-white text-xs leading-6 shadow-md">
       {icon ?? <span>{icon}</span>}
       <span>{children}</span>
     </button>

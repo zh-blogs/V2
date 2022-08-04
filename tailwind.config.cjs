f165c7908f6d798717f1190307f08ceefd9cb225
Justin Sun <i@jsun969.cn>
2022-08-04 22:03:45 +0800

    feat: 移除主题色

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    colors: {
-      primary: '#F2EAE0',
-      secondary: '#F3F0E7',
-    },
-  },
-  plugins: [],
 };

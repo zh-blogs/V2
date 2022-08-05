f77e23628afee87ed0fa414e6614b69fa99a5e46
Justin Sun <i@jsun969.cn>
2022-08-05 10:38:24 +0800

    feat: 添加按钮禁用样式

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,11 +1,15 @@
 interface ButtonProps {
   icon?: JSX.Element;
+  disabled?: boolean;
   children: string;
 }
 
-const Button = ({ icon, children }: ButtonProps) => {
+const Button = ({ icon, children, disabled }: ButtonProps) => {
   return (
-    <button className="w-[90px] h-[25px] transition-colors bg-[#40A9FF] hover:bg-[#2B699C] active:bg-[#8CCBFF] text-white text-xs leading-6 shadow-md">
+    <button
+      disabled={disabled}
+      className="w-[90px] h-[25px] transition-colors bg-[#40A9FF] hover:bg-[#2B699C] active:bg-[#8CCBFF] disabled:bg-[#BFBDB6] text-white disabled:text-[#73716D] text-xs leading-6 shadow-md"
+    >
       {icon ?? <span>{icon}</span>}
       <span>{children}</span>
     </button>

d17d24f7b5f55f9185a9cb4d1077a3d48934e367
Justin Sun <i@jsun969.cn>
2022-08-06 20:13:54 +0800

    feat: 调整tab下划线宽度

--- src/components/elements/TabItem.tsx
+++ src/components/elements/TabItem.tsx
@@ -13,8 +13,8 @@ const TabItem = ({ children, selected, to }: TabItemProps) => {
       className={clsx(
         'text-[18px] font-medium active:border-0 my-4 transition-colors',
         selected
-          ? 'text-[#3C3C3C] border-[#3C3C3C] border-b-2'
-          : 'text-[#9E9E9E] hover:border-[#9E9E9E] hover:border-b-2 active:text-[#3C3C3C]',
+          ? 'text-[#3C3C3C] border-[#3C3C3C] border-b-[1px]'
+          : 'text-[#9E9E9E] hover:border-[#9E9E9E] hover:border-b-[1px] active:text-[#3C3C3C]',
       )}
       to={to}
     >
